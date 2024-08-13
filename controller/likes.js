e351d8eac406eeb33d46d49a9510e739394fb129
Megha30501 <m_patel39@u.pacific.edu>
2024-08-13 15:48:15 -0700

    Added code to get all the likes

--- controller/likes.js
+++ controller/likes.js
@@ -14,9 +14,9 @@ const addLike = async (req, res) => {
       return res.status(400).json({ error: "communityId is required" });
     }
 
-    const likeRef = db.collection("Likes").doc(postId);
+    const likeRef = db.collection("Likes");
 
-    const likeData = await likeRef.set({
+    const likeData = await likeRef.add({
       userId,
       postId: postId || null,
       commentId: commentId || null,
@@ -31,4 +31,26 @@ const addLike = async (req, res) => {
   }
 };
 
-module.exports = { addLike };
+const getLike = async (req, res) => {
+  const userId = req.user.uid;
+
+  try {
+    const likeRef = db.collection("Likes").where("userId", "==", userId);
+    const response = await likeRef.get();
+
+    if (response.empty) {
+      return res.status(404).send("No likes found.");
+    }
+
+    let responseArr = [];
+    response.forEach((doc) => {
+      responseArr.push({ id: doc.id, ...doc.data() });
+    });
+    res.status(200).json(responseArr);
+  } catch (error) {
+    console.error("Error getting like:", error);
+    res.status(500).json({ error: "Failed to get like" });
+  }
+};
+
+module.exports = { addLike, getLike };
--- routes/likes.js
+++ routes/likes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { addLike } = require("../controller/likes");
+const { addLike, getLike } = require("../controller/likes");
 const { varifyToken } = require("../middleware/auth");
 
 router.post("/add-like", varifyToken, addLike);
+router.get("/get-like", varifyToken, getLike);
 
 module.exports = router;
